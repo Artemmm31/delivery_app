d474c256c13306deca624050976133c28d15326b
name <you@example.com>
2025-10-01 18:49:48 +0300

    chore: setup appwrite

---
+++ lib/appWrite.ts
@@ -0,0 +1,5 @@
+export const appWriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: 'com.delivery',
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+}
