ecc7e0eaedce23d6f9f2eedb2205e88d036fd19e
name <you@example.com>
2025-10-01 19:27:54 +0300

    auth screens layout

--- lib/appWrite.ts
+++ lib/appWrite.ts
@@ -1,5 +1,7 @@
 export const appWriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform: 'com.delivery',
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '68dd4dc00037897e87cd',
+    userTableId: '1111',
 }
